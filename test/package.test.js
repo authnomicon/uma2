335052c3e43935ad19ae4d4de4f87a57825ef653
Jared Hanson <jaredhanson@gmail.com>
2018-06-02 12:44:57 -0700

    Add package test.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,25 @@
-/* global describe, it */
+/* global describe, it, expect */
 
-var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('authnomicon-oauth2-uma2', function() {
+describe('@authnomicon/uma2', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/uma2');
+      
+      expect(json.assembly.components).to.have.length(1);
+      expect(json.assembly.components).to.include('http/exchange/ticket/exchange');
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
