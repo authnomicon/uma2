efeb76e4f0a1cf5cc1e4fa755e87e2bb88cfabee
Jared Hanson <jaredhanson@gmail.com>
2018-06-03 13:33:41 -0700

    Rename files.

--- test/http/oauth2/exchange/ticket/exchange.test.js
+++ test/http/oauth2/exchange/ticket/exchange.test.js
@@ -3,7 +3,7 @@
 var $require = require('proxyquire');
 var expect = require('chai').expect;
 var sinon = require('sinon');
-var factory = require('../../../../app/http/exchange/ticket/exchange');
+var factory = require('../../../../../app/http/oauth2/exchange/ticket/exchange');
 
 
 describe('http/exchange/ticket/exchange', function() {
@@ -22,7 +22,7 @@ describe('http/exchange/ticket/exchange', function() {
     var ticketSpy = sinon.stub();
     var issue = function(){};
     
-    var factory = $require('../../../../app/http/exchange/ticket/exchange',
+    var factory = $require('../../../../../app/http/oauth2/exchange/ticket/exchange',
       { 'oauth2orize-uma': { exchange: { ticket: ticketSpy } } });
     var exchange = factory(issue);
     
